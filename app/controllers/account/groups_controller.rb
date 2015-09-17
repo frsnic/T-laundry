fb8ed9bbd812d9d77987b0cda6855eadf1a47536
frsnic <frsnic@gmail.com>
2015-09-17 17:43:47 +0800

    only group_manager can see account/groups index

--- app/controllers/account/groups_controller.rb
+++ app/controllers/account/groups_controller.rb
@@ -1,9 +1,8 @@
 class Account::GroupsController < ApplicationController
   before_action :authenticate_user!
-  before_action :authorize_admin!, except: [:index, :manager_list]
+  before_action :authorize_group_manager!
 
   def index
-    authorize Group, :manager?
     @groups = policy_scope(Group)
   end
 
