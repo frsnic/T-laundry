a96063db2d5d9a39f5a5bb89ce66998626501c34
frsnic <frsnic@gmail.com>
2015-09-19 14:26:17 +0800

    set default timezone to taipei

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module TLaundry
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.time_zone = 'Taipei'
+
   end
 end
