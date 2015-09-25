8c2203205f385a3c98aaf935bf339377e4289d04
frsnic <frsnic@gmail.com>
2015-09-25 13:46:22 +0800

    pundit user_not_authorized action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   protected
 
@@ -25,6 +26,11 @@ class ApplicationController < ActionController::Base
 
   def authorize_group_manager!
     authorize Group, :group_manager?
+  end
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(request.referrer || root_path)
   end
 
 end
