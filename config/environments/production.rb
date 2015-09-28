a300a407831ebe7861424cba44268040dda25dd3
frsnic <frsnic@gmail.com>
2015-09-28 11:19:03 +0800

    update config/environments/production.rb for mail setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,11 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = config_for(:email).symbolize_keys
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -76,8 +80,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.action_mailer.smtp_settings = config_for(:email).symbolize_keys
 
   Rails.application.config.middleware.use ExceptionNotification::Rack,
   :email => {
