61e7e47219610b00da6c10c201f88d74c9d0fc5d
frsnic <frsnic@gmail.com>
2015-10-08 00:34:28 +0800

    update model group spec

---
+++ spec/factories/group.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+
+  factory :group do
+    title Faker::Name.name
+    precision Faker::Number.between(from = 0, to = 2)
+  end
+
+end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,18 +1,30 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe Group do
+RSpec.describe Group, type: :model do
 
-  it "has none to begin with" do
-    expect(Group.count).to eq 0
+  it "has a valid factory" do
+    expect(FactoryGirl.create(:group).valid?).to be true
   end
 
-  it "has one after adding one" do
-    Group.create(title: SecureRandom.hex)
-    expect(Group.count).to eq 1
+  it "is invalid without a title" do
+    expect(FactoryGirl.build(:group, title: nil).valid?).to be false
   end
 
-  it "has none after one was created in a previous example" do
-    expect(Group.count).to eq 0
+  it "does not allow duplicate title per group" do
+    FactoryGirl.create(:group, title: "785-555-1234")
+    expect(FactoryGirl.build(:group, title: "785-555-1234").valid?).to be false
+  end
+
+  it "is invalid without a precision" do
+    expect(FactoryGirl.build(:group, precision: nil).valid?).to be false
+  end
+
+  it "is invalid when precision < 0" do
+    expect(FactoryGirl.build( :group, precision: -1).valid?).to be false
+  end
+
+  it "is invalid when precision > 2" do
+    expect(FactoryGirl.build( :group, precision: 3).valid?).to be false
   end
 
 end
