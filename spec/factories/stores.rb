7be9f98020d3f48cec5d51e1204df24b4732e4de
frsnic <frsnic@gmail.com>
2015-10-08 14:38:00 +0800

    update model store spec

--- spec/factories/stores.rb
+++ spec/factories/stores.rb
@@ -1,6 +1,11 @@
 FactoryGirl.define do
+
   factory :store do
-    title "MyString"
+    title    Faker::Name.name
+    group_id Faker::Number.digit
+    phone    Faker::PhoneNumber.phone_number
+    landline Faker::PhoneNumber.phone_number
+    location Faker::Address.street_address
   end
 
 end
--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -1,17 +1,23 @@
 require "rails_helper"
 
-RSpec.describe StoreUser do
+RSpec.describe Store do
 
   it "has a valid factory" do
-    expect(FactoryGirl.create(:store_user).valid?).to be true
+    expect(FactoryGirl.create(:store).valid?).to be true
   end
 
-  it "is invalid without a store_id" do
-    expect(FactoryGirl.build(:store_user, store_id: nil).valid?).to be false
+  it "is invalid without a title" do
+    expect(FactoryGirl.build(:store, title: nil).valid?).to be false
   end
 
-  it "is invalid without a user_id" do
-    expect(FactoryGirl.build(:store_user, user_id: nil).valid?).to be false
+  it "allow duplicate title in different group per store" do
+    FactoryGirl.create(:store, title: "store_title", group_id: 1)
+    expect(FactoryGirl.build(:store, title: "store_title", group_id: 2).valid?).to be true
+  end
+
+  it "does not allow duplicate title in same group per store" do
+    FactoryGirl.create(:store, title: "store_title", group_id: 1)
+    expect(FactoryGirl.build(:store, title: "store_title", group_id: 1).valid?).to be false
   end
 
 end
