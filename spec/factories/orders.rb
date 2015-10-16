e5f788616abd288385cfbf1896cb557c5a4b1e37
frsnic <frsnic@gmail.com>
2015-10-16 16:08:33 +0800

    update model order spec

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,9 +1,20 @@
 FactoryGirl.define do
+
   factory :order do
-    store_id ""
-client_id ""
-sum ""
-sales_user_id 1
+    store_id  Faker::Number.digit
+    client_id Faker::Number.digit
+    price     Faker::Commerce.price
+    user_id   Faker::Number.digit
+
+    factory :order_with_items do
+      transient do
+        items_count 2
+      end
+
+      after(:create) do |order, evaluator|
+        create_list(:order_item, evaluator.items_count, order: order)
+      end
+    end
   end
 
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,121 +2,127 @@ require 'rails_helper'
 
 RSpec.describe Order, type: :model do
 
-  it "has none to begin with" do
-    expect(Order.count).to eq 0
+  it "has a valid factory" do
+    expect(FactoryGirl.create(:order).valid?).to be true
   end
 
-  it "has one after adding one" do
-    Order.create(store_id: 1, client_id: 1, user_id: 1)
-    expect(Order.count).to eq 1
+  it "is invalid without a store_id" do
+    expect(FactoryGirl.build(:order, store_id: nil).valid?).to be false
   end
 
-  it "has none after one was created in a previous example" do
-    expect(Order.count).to eq 0
+  it "is invalid without a client_id" do
+    expect(FactoryGirl.build(:order, client_id: nil).valid?).to be false
   end
 
-  describe "create with 2 order_items" do
+  it "is invalid without a user_id" do
+    expect(FactoryGirl.build(:order, user_id: nil).valid?).to be false
+  end
 
-    before(:each) do
-      @order = Order.create(store_id: 1, client_id: 1, user_id: 1)
-      OrderItem.create(order_id: @order.id, cloth_title: 'Cloth Title', wash_way: SecureRandom.hex)
-      OrderItem.create(order_id: @order.id, cloth_title: 'Cloth Title', wash_way: SecureRandom.hex)
-    end
+  it "is invalid without a price" do
+    expect(FactoryGirl.build(:order, price: nil).valid?).to be false
+  end
 
-    describe ", order initialize" do
+  it "is invalid when price greater than 100000" do
+    expect(FactoryGirl.build(:order, price: 500000).valid?).to be false
+  end
 
-      it ", order status should be processing" do
-        expect(@order.status).to eq "processing"
+  it "is invalid when price less than 0" do
+    expect(FactoryGirl.build(:order, price: -50).valid?).to be false
+  end
+
+  describe "#create order_with_items," do
+
+    let(:order) { FactoryGirl.create :order_with_items }
+
+    context "initialize," do
+
+      it "order status should be processing" do
+        expect(order.status).to eq "processing"
       end
 
-      it ", fetched_at should be length=0 string" do
-        expect(@order.fetched_at).to eq ""
+      it "fetched_at should be length=0 string" do
+        expect(order.fetched_at).to eq ""
       end
 
     end
 
-    describe ", order_item first finish" do
+    context "order_item first finish," do
 
       before(:each) do
-        @order.order_items.first.update(status: OrderItem.statuses[:finish])
-        @order = Order.first
+        order.order_items.first.update(status: OrderItem.statuses[:finish])
       end
 
-      it ", order status should be processing" do
-        expect(@order.status).to eq "processing"
+      it "order status should be processing" do
+        expect(order.status).to eq "processing"
       end
 
-      it ", fetched_at should be length=0 string" do
-        expect(@order.fetched_at).to eq ""
-      end
-
-    end
-
-    describe ", order_item all finish" do
-
-      before(:each) do
-        @order.order_items.update_all(status: OrderItem.statuses[:finish])
-        @order = Order.first
-      end
-
-      it ", order status should be processing" do
-        expect(@order.status).to eq "finish"
-      end
-
-      it ", fetched_at should be length=0 string" do
-        expect(@order.fetched_at).to eq ""
+      it "fetched_at should be length=0 string" do
+        expect(order.fetched_at).to eq ""
       end
 
     end
 
-    describe ", order_item first out" do
+    context "order_item all finish," do
 
       before(:each) do
-        @order.order_items.first.update(status: OrderItem.statuses[:out], fetched_at: Time.now())
-        @order = Order.first
+        order.order_items.update_all(status: OrderItem.statuses[:finish])
       end
 
-      it ", order status should be processing" do
-        expect(@order.status).to eq "processing"
+      it "order status should be processing" do
+        expect(order.status).to eq "finish"
       end
 
-      it ", fetched_at should be length=0 string" do
-        expect(@order.fetched_at).to eq ""
+      it "fetched_at should be length=0 string" do
+        expect(order.fetched_at).to eq ""
       end
 
     end
 
-    describe ", order_item one finish, one out" do
+    context "order_item first out," do
 
       before(:each) do
-        @order.order_items.first.update(status: OrderItem.statuses[:finish])
-        @order.order_items.last.update( status: OrderItem.statuses[:out], fetched_at: Time.now())
-        @order = Order.first
+        order.order_items.first.update(status: OrderItem.statuses[:out], fetched_at: Time.now())
       end
 
-      it ", order status should be processing" do
-        expect(@order.status).to eq "finish"
+      it "order status should be processing" do
+        expect(order.status).to eq "processing"
       end
 
-      it ", fetched_at should be length=0 string" do
-        expect(@order.fetched_at).to eq ""
+      it "fetched_at should be length=0 string" do
+        expect(order.fetched_at).to eq ""
       end
 
     end
 
-    describe ", order_item all out" do
+    context "order_item one finish, one out," do
 
       before(:each) do
-        @order.order_items.update_all(status: OrderItem.statuses[:out], fetched_at: Time.now())
-        @order = Order.first
+        order.order_items.first.update(status: OrderItem.statuses[:finish])
+        order.order_items.last.update( status: OrderItem.statuses[:out], fetched_at: Time.now())
       end
 
-      it ", order status should be processing" do
-        expect(@order.status).to eq "out"
+      it "order status should be processing" do
+        expect(order.status).to eq "finish"
       end
 
-      it ", fetched_at should be length=0 string" do
-        expect(@order.fetched_at.class).to eq ActiveSupport::TimeWithZone
+      it "fetched_at should be length=0 string" do
+        expect(order.fetched_at).to eq ""
+      end
+
+    end
+
+    context "order_item all out," do
+
+      before(:each) do
+        order.order_items.update_all(status: OrderItem.statuses[:out], fetched_at: Time.now())
+      end
+
+      it "order status should be processing" do
+        expect(order.status).to eq "out"
+      end
+
+      it "fetched_at.class should be ActiveSupport::TimeWithZone" do
+        expect(order.fetched_at.class).to eq ActiveSupport::TimeWithZone
       end
 
     end
