a5e07e2cad7b16aea23383026aab3d9b62c55037
Hank <frsnic@gmail.com>
2016-01-26 23:23:14 +0800

    faker unique

--- spec/factories/clients.rb
+++ spec/factories/clients.rb
@@ -3,11 +3,11 @@ require 'faker'
 FactoryGirl.define do
 
   factory :client do
-    name     Faker::Name.name
-    store_id Faker::Number.digit
-    balance  Faker::Commerce.price
-    phone    Faker::PhoneNumber.phone_number
-    address  Faker::Address.street_address
+    name     { Faker::Name.name }
+    store_id { Faker::Number.digit }
+    balance  { Faker::Commerce.price }
+    phone    { Faker::PhoneNumber.phone_number }
+    address  { Faker::Address.street_address }
   end
 
 end
